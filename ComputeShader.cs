68cedce7311ada9b493f09c98908f366a0a9ed45
Yuting Han <49219934+rainstophan@users.noreply.github.com>
2022-09-01 15:53:30 +0200

    Update ComputeShader.cs

--- ComputeShader.cs
+++ ComputeShader.cs
@@ -1,1 +1,3 @@
+# Aim: Compute shaders are programs that allow us to execute arbitrary code on the GPU. Workgroups(3D) - > Threads(3D). To write data to the GPU, we need a compute buffer to act as middle man. 
+# Video https://www.youtube.com/watch?v=BrZ4pWwkpto
 # Link https://polycoding.net/compute-shaders-unity#compute-buffers-shaders-in-unity
